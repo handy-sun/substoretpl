30ee74d40c0f81920f55fed853b0283683e03b42
sooncheer <handy-sun@foxmail.com>
2026-02-06 20:03:15 +0800

    refine about args parse, and auto_redirect not ava in MacOS

--- sing-box.js
+++ sing-box.js
@@ -1,4 +1,11 @@
-const { type, name, istun } = $arguments
+const args =
+  typeof $arguments !== 'undefined'
+    ? $arguments
+    : {
+        type: 0,
+        name: '_',
+        istun: 0,
+      }
 const compatible_outbound = {
   tag: 'COMPATIBLE',
   type: 'direct',
@@ -8,14 +15,24 @@ const tun_inbound = {
   tag: 'tun-in',
   address: [ '172.19.0.1/30', 'fdfe:dcba:9876::1/126' ],
   auto_route: true,
-  auto_redirect: true,
   strict_route: true,
 }
 
+function sblog(v) {
+  console.log(`[📦sing-box] ${v}`)
+}
 function getTags(proxies, regex) {
   return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
 }
 
+sblog('🚀 开始 args:', args)
+// 如果是直接在软件中粘贴脚本的，就手动修改下面这几个变量实现自定义配置
+let {
+  type = args.type || 0,
+  name = args.name || '_',
+  istun = /^1$|true/i.test(args.istun) ? true : false,
+} = args
+sblog(`传入参数 type: ${type}, name: ${name}, istun: ${istun}`)
 let config = JSON.parse($files[0])
 let proxies = await produceArtifact({
   name,
@@ -42,13 +59,14 @@ config.outbounds.forEach(outbound => {
       config.outbounds.push(compatible_outbound)
       compatible = true
     }
-    outbound.outbounds.push(compatible_outbound.tag);
+    outbound.outbounds.push(compatible_outbound.tag)
   }
 });
 
-let tun_flag = /^1$|true/i.test(istun) ? true : false
-if (tun_flag) {
+if (istun) {
   config.inbounds.push(tun_inbound)
+  config.route.rules[0].inbound = 'tun-in'
+  sblog(`route.rules[0]更新为: ${JSON.stringify(config.route.rules[0])}`)
 }
 
 $content = JSON.stringify(config, null, 2)
